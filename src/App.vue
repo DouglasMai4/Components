f8a49272a04e86298ec0101d5bc5540b9f60b8fd
Douglas Maia <douglasmai4@protonmail.com>
2023-12-30 12:49:30 -0300

    feat: Initial Layout

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,29 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <main>
+    <section>
+      <h1 class="text-primary">Custom Components VUE</h1>
+    </section>
+  </main>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<style scoped lang="scss">
+  @import './assets/colors.scss';
+
+  main {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    min-height: 100dvh;
+
+    section {
+      padding: 1rem;
+      border-radius: 10px;
+      background-color: $bg-color-s;
+      border: 1px solid black;
+      box-shadow: 5px 5px 15px rgb(18 18 18 / 50%);
+    }
+  }
 </style>
