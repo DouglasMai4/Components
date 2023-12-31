18c65220e21da25974f9a7161af1b70f7052acab
Douglas Maia <douglasmai4@protonmail.com>
2023-12-31 16:19:01 -0300

    feat: Created Input component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,42 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+
+import Input from './components/Input.vue';
+
+const name = ref('');
+const password = ref('');
 </script>
 
 <template>
   <main>
     <section>
-      <h1 class="text-primary">Custom Components VUE</h1>
+      <h1 class="text-primary">Formulário</h1>
+
+      <hr />
+
+      <Input
+        id="name"
+        icon="ph:user"
+        placeholder="Nome Completo"
+        v-model="name"
+      />
+
+      <Input
+        id="password"
+        icon="ph:lock"
+        placeholder="Senha"
+        type="password"
+        v-model="password"
+      />
+    </section>
+
+    <section>
+      <h1 class="text-secondary">Dados</h1>
+
+      <hr />
+
+      Nome: <span>{{ name }}</span>
+      Senha: <span>{{ password }}</span>
     </section>
   </main>
 </template>
@@ -19,11 +51,20 @@
     min-height: 100dvh;
 
     section {
+      display: flex;
+      flex-direction: column;
+      gap: 0.5rem;
       padding: 1rem;
-      border-radius: 10px;
+      border-radius: 10px 0 0 10px;
       background-color: $bg-color-s;
       border: 1px solid black;
       box-shadow: 5px 5px 15px rgb(18 18 18 / 50%);
+      width: min(25rem, 100%);
+      aspect-ratio: 2 / 1;
+
+      &:last-child {
+        border-radius: 0 10px 10px 0;
+      }
     }
   }
 </style>
---
+++ src/components/Input.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { Icon } from '@iconify/vue'
+import styles from './Input.module.scss'
+
+const emit = defineEmits(['update:modelValue'])
+</script>
+
+<template>
+  <label :for="id" :class="styles.label">
+    <Icon :icon="icon || 'ph:pen'" :class="styles.icon" />
+
+    <input
+      :id="id"
+      v-bind="$attrs"
+      :type="type"
+      :class="styles.input"
+      :value="modelValue"
+      @input="emit('update:modelValue', ($event.target as HTMLInputElement).value)"
+    >
+  </label>
+</template>
+
+<script lang="ts">
+  export default {
+    props: {
+      icon: String,
+      id: String,
+      type: String,
+      modelValue: String,
+    }
+  }
+</script>
